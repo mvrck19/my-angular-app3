94900014530517d637850c4b4ab125acc12fcc02
mvrc <phaidos@gmail.com>
2024-06-24 22:44:14 +0300

    feat: Add Leaflet library for map functionality
    The code changes include adding the Leaflet library to the project dependencies in both the `package.json` and `package-lock.json` files. This library will be used to implement map functionality in the application.

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
+export class ContactComponent  {
 
 }
