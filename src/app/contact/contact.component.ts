6d8a725938874545a4562cb337e1a904e774d909
mvrc <p.haidos@gmail.com>
2025-05-11 16:07:06 +0300

    contact update

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, MatChipsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
