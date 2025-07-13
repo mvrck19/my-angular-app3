7be50efaf83d52e25f3f9aa76bedc4b7a9e58cc4
mvcr <p.haidos@gmail.com>
2025-07-13 12:27:25 +0300

    vaso_updates

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,23 +2,58 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatCardModule } from '@angular/material/card';
 
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [MatIconModule, CommonModule, MatChipsModule],
+  imports: [MatIconModule, CommonModule, MatChipsModule, MatCardModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-  specialties = [
-    { icon: 'local_hospital', text: 'Πρώτες Βοήθειες' },
-    { icon: 'fitness_center', text: 'Άσκηση στην Ορθοπαιδική Αποκατάσταση' },
-    { icon: 'healing', text: 'Αντιμετώπιση Οιδημάτων & Λεμφική Παροχέτευση' },
-    { icon: 'favorite', text: 'Καρδιοαναπνευστικές Φυσικοθεραπευτικές Προσεγγίσεις' },
-    { icon: 'accessibility', text: 'Αντιμετώπιση Πόνου στον Ώμο' },
-    { icon: 'self_improvement', text: 'Θεραπευτικό Pilates' },
-    { icon: 'health_and_safety', text: 'Φυσικοθεραπεία Πυελικού Εδάφους' }
+
+  educationList = [
+    {
+      year: '2022',
+      title: 'Άσκηση στην Ορθοπαιδική Αποκατάσταση',
+      institution: 'Manual Courses'
+    },
+    {
+      year: '2024',
+      title: 'Ολοκληρωμένη Διαχείριση Οιδήματος με τη Μέθοδο Vodder',
+      institution: 'Epimorfosis'
+    },
+    {
+      year: '2024',
+      title: 'Φυσικοθεραπευτική Αντιμετώπιση Αναπνευστικών Προβλημάτων',
+      institution: 'Πανελλήνιος Σύλλογος Φυσικοθεραπευτών'
+    },
+    {
+      year: '2024',
+      title: 'Θεραπεύτρια Μασάζ',
+      institution: 'Release and Relax Motion'
+    },
+    {
+      year: '2024',
+      title: 'The Painful Shoulder',
+      institution: 'Adam Meakins'
+    },
+    {
+      year: '2024',
+      title: 'Clinical Pilates',
+      institution: 'Clinical Pilates Institute'
+    },
+    {
+      year: '2025',
+      title: 'Φυσικοθεραπεία Πυελικού Εδάφους',
+      institution: 'Πανεπιστήμιο Πατρών'
+    },
+    {
+      year: '2017 - 2018 - 2025',
+      title: 'Σεμινάρια Πρώτων Βοηθειών',
+      institution: ''
+    },
   ];
 }
